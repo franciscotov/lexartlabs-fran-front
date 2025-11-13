40d66b5179782b48613238fd8c9030cf3454e2ec
franciscotov <franciscodejesus.tovarceballo.contractor@gdevtools.com>
2025-11-13 15:36:38 -0300

    adding spinner

--- src/pages/product/list/ViewModel.ts
+++ src/pages/product/list/ViewModel.ts
@@ -1,25 +1,19 @@
-import { ProductDto } from "@/lib/definitions";
 import useAlert from "@/components/alert/AlertComponent";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchGetProductList } from "@/redux/actions/productActions";
 import { ProductState, StateI } from "@/redux/types/types";
-import i18n from "@/i18n/i18n-es.json";
 import { useEffect } from "react";
 
 const ViewModel = () => {
   const dispatch = useDispatch();
-  const { AlertComponent, openSnackbar } = useAlert();
+  const { AlertComponent } = useAlert();
   const products: ProductState = useSelector<StateI>(
     (state) => state.products
   ) as ProductState;
-  const data = useSelector<StateI>((state) => state.data);
-  console.log(data)
-  const getProducts = async () => {
-    dispatch<any>(fetchGetProductList());
-  };
+
   useEffect(() => {
-    getProducts();
-  }, []);
+    dispatch<any>(fetchGetProductList());
+  }, [dispatch]);
 
   return {
     AlertComponent,
--- src/pages/product/list/index.tsx
+++ src/pages/product/list/index.tsx
@@ -1,8 +1,7 @@
-import React, { Suspense } from "react";
-import { containerStyles } from "@/pages/signup/contants";
 import ViewModel from "./ViewModel";
 import { ProductDto } from "@/lib/definitions";
 import { RequestState } from "@/components/RequestStatus";
+import { RotatingLines } from "react-loader-spinner";
 
 const ProductList = () => {
   const { products } = ViewModel();
@@ -10,7 +9,21 @@ const ProductList = () => {
   return (
     <RequestState status={products.status}>
       <RequestState.Loading>
-        <p>Loading products...</p>
+        <RotatingLines
+          visible={true}
+          height="96"
+          width="96"
+          color="blue"
+          strokeWidth="5"
+          ariaLabel="rotating-lines-loading"
+          wrapperStyle={{
+            justifyContent: "center",
+            alignItems: "center",
+            display: "flex",
+            marginTop: "120px",
+          }}
+          wrapperClass=""
+        />
       </RequestState.Loading>
       <RequestState.Error>
         <p>Error loading products. Please try again later.</p>
